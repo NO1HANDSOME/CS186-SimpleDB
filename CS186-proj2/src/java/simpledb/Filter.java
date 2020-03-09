2f6753b5d435568970b362234e323475429e85b1
NO1HANDSOME <411032593@qq.com>
2020-03-09 22:52:07 +0800

    2020/3/9

--- CS186-proj2/src/java/simpledb/Filter.java
+++ CS186-proj2/src/java/simpledb/Filter.java
@@ -7,68 +7,81 @@ import java.util.*;
  */
 public class Filter extends Operator {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Constructor accepts a predicate to apply and a child operator to read
-     * tuples to filter from.
-     * 
-     * @param p
-     *            The predicate to filter tuples with
-     * @param child
-     *            The child operator
-     */
-    public Filter(Predicate p, DbIterator child) {
-        // some code goes here
-    }
+	private Predicate p;
 
-    public Predicate getPredicate() {
-        // some code goes here
-        return null;
-    }
+	private DbIterator child;
 
-    public TupleDesc getTupleDesc() {
-        // some code goes here
-        return null;
-    }
+	/**
+	 * Constructor accepts a predicate to apply and a child operator to read tuples
+	 * to filter from.
+	 * 
+	 * @param p     The predicate to filter tuples with
+	 * @param child The child operator
+	 */
+	public Filter(Predicate p, DbIterator child) {
+		// some code goes here
+		this.p = p;
+		this.child = child;
+	}
 
-    public void open() throws DbException, NoSuchElementException,
-            TransactionAbortedException {
-        // some code goes here
-    }
+	public Predicate getPredicate() {
+		// some code goes here
+		return p;
+	}
 
-    public void close() {
-        // some code goes here
-    }
+	public TupleDesc getTupleDesc() {
+		// some code goes here
+		return child.getTupleDesc();
+	}
 
-    public void rewind() throws DbException, TransactionAbortedException {
-        // some code goes here
-    }
+	public void open() throws DbException, NoSuchElementException, TransactionAbortedException {
+		// some code goes here
+		super.open();
+		child.open();
+	}
 
-    /**
-     * AbstractDbIterator.readNext implementation. Iterates over tuples from the
-     * child operator, applying the predicate to them and returning those that
-     * pass the predicate (i.e. for which the Predicate.filter() returns true.)
-     * 
-     * @return The next tuple that passes the filter, or null if there are no
-     *         more tuples
-     * @see Predicate#filter
-     */
-    protected Tuple fetchNext() throws NoSuchElementException,
-            TransactionAbortedException, DbException {
-        // some code goes here
-        return null;
-    }
+	public void close() {
+		// some code goes here
+		child.close();
+		super.close();
+	}
 
-    @Override
-    public DbIterator[] getChildren() {
-        // some code goes here
-        return null;
-    }
+	public void rewind() throws DbException, TransactionAbortedException {
+		// some code goes here
+		child.rewind();
+	}
 
-    @Override
-    public void setChildren(DbIterator[] children) {
-        // some code goes here
-    }
+	/**
+	 * AbstractDbIterator.readNext implementation. Iterates over tuples from the
+	 * child operator, applying the predicate to them and returning those that pass
+	 * the predicate (i.e. for which the Predicate.filter() returns true.)
+	 * 
+	 * @return The next tuple that passes the filter, or null if there are no more
+	 *         tuples
+	 * @see Predicate#filter
+	 */
+	protected Tuple fetchNext() throws NoSuchElementException, TransactionAbortedException, DbException {
+		// some code goes here
+		while (child.hasNext()) {
+			Tuple temp = child.next();
+			if (p.filter(temp))
+				return temp;
+		}
+		return null;
+	}
+
+	@Override
+	public DbIterator[] getChildren() {
+		// some code goes here
+		return new DbIterator[] { child };
+	}
+
+	@Override
+	public void setChildren(DbIterator[] children) {
+		// some code goes here
+		child = children[0];
+	}
 
 }
